8fb492dc965d9913f84eebee60a847a23559d780
simonkennedy468 <simonkennedy468@gmail.com>
2022-12-20 23:20:20 +0000

    getting grabbing and throwing working

--- Assignment/Assets/Grab.cs
+++ Assignment/Assets/Grab.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.InputSystem;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    List<GameObject> objects = new List<GameObject>();
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(XR)
+        //if (OVRInput.Get(OVR.Rawbutton.LHandTrigger)) ;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        objects.Add(other.gameObject);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        objects.Remove(other.gameObject);
     }
 }
